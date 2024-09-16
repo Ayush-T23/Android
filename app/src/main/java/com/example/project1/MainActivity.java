52543cdc29053c99fd26ae1e951d94c919cb0e54
ayushp1 <tiwariayush7487@gmail.com>
2024-09-16 14:36:55 +0530

    finished project

---
+++ app/src/main/java/com/example/project1/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.project1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+ TextView t1;
+ Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        t1=findViewById(R.id.text1);
+        b1=findViewById(R.id.btn1);
+        b1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v ){
+                t1.setText("Hiii Ayush Tiwari How Are Uh");
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+}
